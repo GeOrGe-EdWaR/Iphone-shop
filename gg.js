0e954adeb7a4e438111957095b0f6c6f60404940
GeOrGe-EdWaR <131381073+GeOrGe-EdWaR@users.noreply.github.com>
2023-05-01 09:32:30 +0200

    Add files via upload

---
+++ gg.js
@@ -0,0 +1,8 @@
+let main = document.getElementById("main");
+let container = document.getElementById("container");
+function phones(phone) {
+  main.src = phone;
+};
+function colors(color) {
+    container.style.background = color;
+}
